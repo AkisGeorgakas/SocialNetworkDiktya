17711f300fbba3580c64d35ab6b04284ffb17346
vml <vml@entersoft.gr>
2025-04-29 20:03:38 +0300

    compiler

---
+++ javaCompinle.bat
@@ -0,0 +1,20 @@
+@echo off
+echo Compiling all Java files...
+
+javac ^
+    src\client\Client.java ^
+    src\server\ClientHandler.java ^
+    src\server\Message.java ^
+    src\server\Server.java ^
+    src\server\SocialGraphLoader.java ^
+    src\server\UsersLoader.java ^
+    src\ServerMain.java ^
+	src\common\Packet.java
+
+IF %ERRORLEVEL% EQU 0 (
+    echo Compilation successful!
+) ELSE (
+    echo Compilation failed! Check the errors above.
+)
+
+pause
